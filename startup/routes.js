d265579e6966837c67cf42abd2a2542daf11a925
Parveen Chauhan <parveen@acewebx.com>
2022-09-07 15:22:06 +0530

    Enable cross browser api call

--- startup/routes.js
+++ startup/routes.js
@@ -1,8 +1,21 @@
 const express = require('express');
+const cors = require("cors")
 const error = require('../middleware/error');
 
 
 module.exports = function(app) {
+    app.use(cors({ origin: '*' }));
+    app.all("/*", function (req, res, next) {
+        res.header("Access-Control-Allow-Origin", req.headers.origin);
+        res.header("Access-Control-Allow-Credentials",true);
+        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+        res.header('Access-Control-Allow-Headers', 'Content-Type,Accept,X-Access-Token,X-Key,Authorization,X-Requested-With,Origin,Access-Control-Allow-Origin,Access-Control-Allow-Credentials');
+        if (req.method === 'OPTIONS') {
+            res.status(200).end();
+        } else {
+            next();
+        }
+    });
     app.get("/", (req, res) => {
         res.json({ message: "Welcome to AceWebX.com application." });
     });
